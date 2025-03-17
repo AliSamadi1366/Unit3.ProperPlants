269b67b7c4bf3f3fe7e98a9097277332bad04f9a
Ali Samadi <alisamadi.096@gmail.com>
2025-03-17 12:12:24 -0500

    App.jsx is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+import Plants from "./Plants";
+import Cart from "./cart";
+import "./index.css";
 export default function App() {
-  return <></>;
+  const [cart, setCart] = useState([]);
+
+  // Function to add a plant to the cart
+  const addToCart = (plant) => {
+    const itemExists = cart.find((i) => i.id === plant.id);
+    if (itemExists) {
+      setCart(
+        cart.map((item) =>
+          item.id === plant.id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+      const item = { ...plant, quantity: 1 };
+      setCart([...cart, item]);
+    }
+  };
+
+  // Function to remove a plant from the cart
+  const removeFromCart = (itemToRemove) => {
+    setCart(
+      cart
+        .map((item) =>
+          item.id === itemToRemove.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
+  return (
+    <>
+      <h1>Proper Plants</h1>
+      <main>
+        <Plants addToCart={addToCart} />
+        <Cart
+          cart={cart}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
+      </main>
+    </>
+  );
 }
